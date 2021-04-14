e4cb569b58bfd2182f239110e70715f9aa2add7a
Raj Shringi <raj24shringi@gmail.com>
2021-04-15 01:13:49 +0530

    add freecodecamp sol-1 in caesarsCiphe.js

--- freecodecamp/javascript-projects/caesarsCiphe.js
+++ freecodecamp/javascript-projects/caesarsCiphe.js
@@ -17,3 +17,16 @@ function rot13(str) {
   }
   
   rot13("SERR YBIR?");
+
+
+// freecodecamp sol-1
+function rot13(str) {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return str
+    .split('')
+    .map(char => {  
+      const pos = alphabet.indexOf(char);      
+      return pos >= 0 ? alphabet[(pos + 13) % 26] : char;
+    })
+    .join('');
+}
